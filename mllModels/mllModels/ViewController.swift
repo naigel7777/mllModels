903810592dae048188a05afa1e8054ea49cf48af
naigel7777 <56576758+naigel7777@users.noreply.github.com>
2020-04-10 12:31:37 +0500

    create new project

---
+++ mllModels/mllModels/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mllModels
+//
+//  Created by Nail Safin on 10.04.2020.
+//  Copyright © 2020 Nail Safin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
